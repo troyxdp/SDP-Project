346c10b6aa2b46e903dfddc1e1f9bb04f2e99a0c
Troy du Plooy <troydp7@gmail.com>
2023-09-20 19:28:05 +0200

    Got transitions working, got basics of login and registration
    - Got the transitioning between pages working.
    - Added the basic components of a log in page using stuff from SD project.
    - Added basic components of a sign up page using stuff from SD project.
    - Made transitions between sign up and log in pages.
    - Added a transition from log in page to profile page.
    - Haven't yet added any interaction with the database, although I updated the firebase.js file by copying some of the code from my last assignment, so the basics are in place

--- project/src/firebase-config/firebase.js
+++ project/src/firebase-config/firebase.js
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,6 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-export {app};
+export {firebaseConfig}
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db =  getFirestore(app);
