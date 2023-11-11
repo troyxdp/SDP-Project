811d91ded9524dee483390ab8ee5374c2993dc92
Deku-Soma <55653619+Deku-Soma@users.noreply.github.com>
2023-11-11 18:13:06 +0200

    Added Imageupload class as a component

--- project/src/components/Imageupload.js
+++ project/src/components/Imageupload.js
@@ -5,5 +5,45 @@ import {
   getDownloadURL,
   listAll,
 } from "../firebase-config/firebase/storage";
-import { db } from '../firebase-config/firebase';
 import { v4 } from "uuid";
+
+export default function imageUpload() {
+    const [imageUpload, setImageUpload] = useState(null);
+    const [imageUrls, setImageUrls] = useState(new Set()); // Use a Set to store unique URLs
+  
+    const imagesListRef = ref(storage, "images/");
+    const uploadFile = () => {
+      if (imageUpload == null) return;
+      const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+      uploadBytes(imageRef, imageUpload).then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((url) => {
+          setImageUrls(prev => new Set([...prev, url])); // Add URL to the Set
+        });
+      });
+    };
+  
+    useEffect(() => {
+      listAll(imagesListRef).then((response) => {
+        response.items.forEach((item) => {
+          getDownloadURL(item).then((url) => {
+            setImageUrls(prev => new Set([...prev, url])); // Add URL to the Set
+          });
+        });
+      });
+    }, []);
+  
+    return (
+      <div className="ImageUpload">
+        <input
+          type="file"
+          onChange={(event) => {
+            setImageUpload(event.target.files[0]);
+          }}
+        />
+        <button onClick={uploadFile}> Upload Image</button>
+        {[...imageUrls].map((url, index) => (
+          <img key={index} src={url} alt={`image-${index}`} />
+        ))}
+      </div>
+    );
+  }
