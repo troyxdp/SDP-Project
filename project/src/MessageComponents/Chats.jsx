63c2ed6b8169f43d0c81fe42cd40870a50856b24
dean-south <solomon.mdean@gmail.com>
2023-11-14 14:23:34 +0200

    Update Chats.jsx

--- project/src/MessageComponents/Chats.jsx
+++ project/src/MessageComponents/Chats.jsx
@@ -43,7 +43,7 @@ const Chats = () => {
           key={chat[0]}
           onClick={() => handleSelect(chat[1].userInfo)}
         >
-          {chat[1].userInfo.profilePic && <img src={chat[1].userInfo.profilePic} alt="" />}
+          {chat[1].userInfo.photoURL && <img src={chat[1].userInfo.photoURL} alt="" />}
           <div className="userChatInfo">
             <span>{chat[1].userInfo.displayName}</span>
             <p>{chat[1].lastMessage?.text}</p>
